960fd695ce5b641c3d4b7729fe74daf2f1c99b55
Home <e.jawadfazza@gmail.com>
2023-12-23 20:06:23 +0300

    .

---
+++ WebService/Controllers/ImageUploadController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
+using System;
+
+namespace WebService.Controllers
+{
+    [Route("api/ImageUpload")]
+    [ApiController]
+    public class ImageUploadController : ControllerBase
+    {
+        private const string UploadDirectory = "Uploads"; // Directory to store uploaded images
+
+        [HttpPost]
+        public async Task<IActionResult> UploadImage()
+        {
+            try
+            {
+                if (Request.Form.Files!=null)
+                {
+                    var file = Request.Form.Files[0]; // Assuming only one file is uploaded
+
+                    if (file.Length > 0)
+                    {
+                        var filePath = Path.Combine(Directory.GetCurrentDirectory(), UploadDirectory, file.FileName);
+
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await file.CopyToAsync(stream);
+                        }
+
+                        return Ok("Image uploaded successfully.");
+                    }
+                }
+
+                return BadRequest("No image uploaded.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+    }
+}
